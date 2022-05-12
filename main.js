d2b0766d01931571a7823e853c0d49bc7da72e3d
또띠 <emilyjang@jangsoyeong-ui-MacBookAir.local>
2022-05-12 21:42:50 +0900

    third commit

--- main.js
+++ main.js
@@ -1,81 +1,71 @@
-//랜덤번호 지정
-//유저가 번호를 입력 그리고 go버튼 누름
-//맞추면 맞췄습니다.
-//랜덤번호 < 유저번호 down
-//랜덤번호 > 유저번호 up
-//리셋 버튼 누르면 게임이 리셋
-//5번의 기회를 다 쓰면 게임이 끝난다(버튼 disable)
-//유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. 기회를 깎지 않음
-//이미 입력한 숫자 또 입력하면 알려준다. 기회를 깎지 않음
+let randomNum = Math.floor(Math.random()*100)+1;
+console.log("정답은? " + randomNum);
+let userInput = document.getElementById("user-input"); //user의 값
+let playButton = document.getElementById("play-button") ;//play 버튼값
+let resultArea = document.getElementById("result-area"); //결과값
+let history = []; //중복검사용 배열
+let chanceArea = document.getElementById("chance-area") ;//남은 기회
+let chances = 5; //기회값
+let num = document.getElementById("num")
 
-let computerNum = 0
-let playButton = document.getElementById("play-button");
-let userInput = document.getElementById("user-input");
-let resultArea = document.getElementById("result-area");
-let resetButton = document.getElementById("reset-button");
-let chances = 5;
-let gameOver = false;
-let chanceArea = document.getElementById("chance-area")
-let history = [];
-
-playButton.addEventListener("click",play)
-resetButton.addEventListener("click",reset)
-userInput.addEventListener("focus",function(){userInput.value=""})
-
-function pickRandomNum(){
-    computerNum = Math.floor(Math.random()*100)+1;
-    console.log("정답: " + computerNum);
-}
+playButton.addEventListener("click",play);
 
 function play(){
-    let userValue = userInput.value;
+    let userValue = userInput.value; //사용자 값
+    let bool = true;
 
-    //유효성 검사 1
-    if(userValue <1 || userValue >100){
-        resultArea.textContent = "1과 100사이의 숫자를 입력해 주세요."
+    //유효성 검사1 :: 범위 밖의 숫자
+    if(userValue > 101 || userValue < 1){
+        resultArea.innerHTML = "1과 100 사이의 숫자를<br> 입력해 주세요";
+        resultArea.style.color = "red";
+        resultArea.style.animation = "shake 300ms";
+        bool = false;
         return;
     }
-    
-    //유효성 검사 2
+
+    //유효성 검사2 :: 중복 숫자
     if(history.includes(userValue)){
-        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요."
+        resultArea.textContent = "이미 입력한 숫자입니다";
+        resultArea.style.color = "red";
+        resultArea.style.animation = "shake 300ms";
+        bool = false;
         return;
-    }
+        }
 
-    chances --;
-    chanceArea.textContent = `남은 기회: ${chances}회`
-    console.log(chances);
-
-    if(userValue < computerNum){
-        resultArea.textContent = "UP!"
-    }else if(userValue > computerNum){
-        resultArea.textContent = "DOWN!"
+    //입력받은 숫자 확인
+    if(userValue < randomNum){
+        resultArea.textContent = "UP";
+        resultArea.style.color = "#996633";
+    }else if(userValue > randomNum){
+        resultArea.textContent = "DOWN";
+        resultArea.style.color = "#996633";
     }else{
-        resultArea.textContent = "CORRECT!"
-        gameOver = true;
-    }
-
-    history.push(userValue)
-
-    if(chances < 1){
-        gameOver = true
-    }
-
-    if(gameOver == true){
+        resultArea.textContent = "CORRECT!!!";
+        resultArea.style.color = "#ffcc00";
         playButton.disabled = true;
+        playButton.style.color ="#ffffff";
+        playButton.style.background = "#444444";
+    }
+
+    if(bool){
+        history.push(userValue); //배열에 숫자 저장
+        console.log(history);
+        chances --;
+        num.textContent = `${chances}`;
     }
 }
 
 function reset(){
-    //user input창 정리
     userInput.value = "";
-    //새로운 번호 생성
-    pickRandomNum();
-    resultArea.textContent = "숫자를 입력해주세요";
     chances = 5;
-    chanceArea.textContent = `남은 기회: ${chances}회`
+    bool = true;
+    num.textContent = `${chances}`;
+    playButton.style.color = "#ffffff";
+    playButton.style.background = "#996633";
     playButton.disabled = false;
+    resultArea.innerHTML = "숫자를 입력해주세요";
+    resultArea.style.color = "#996633";
     history = new Array();
+    randomNum = Math.floor(Math.random()*100)+1;
+    console.log("정답은? " + randomNum);
 }
-
-pickRandomNum()
